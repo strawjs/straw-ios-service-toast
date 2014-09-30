812404015ba3666d9e92bdea82694198ce2f264e
Yoshiya Hinosawa <stibium121@gmail.com>
2014-09-30 23:03:27 +0100

    Add tests

--- Podfile
+++ Podfile
@@ -2,3 +2,5 @@ platform :ios, '6.0'
 
 pod 'Straw', '~> 0.5.3'
 pod 'Toast', '~> 2.3'
+
+pod 'OCMockito'
--- StrawServiceToastTests/StrawServiceToastTests.m
+++ StrawServiceToastTests/StrawServiceToastTests.m
@@ -1,14 +1,49 @@
 #import <XCTest/XCTest.h>
 #import "StrawServiceToast.h"
 
+#define HC_SHORTHAND
+#import <OCHamcrest/OCHamcrest.h>
+
+#define MOCKITO_SHORTHAND
+#import <OCMockito/OCMockito.h>
+
 @interface StrawServiceToastTests : XCTestCase
 
 @end
 
 @implementation StrawServiceToastTests
 
-- (void)testExample
+- (void)testName
 {
+    id<STWService> service = [[StrawServiceToast alloc] init];
+
+    XCTAssertEqual(@"toast", [service name], @"Service name should be toast.");
+}
+
+- (void)testIsBackgroundJob
+{
+    id<STWService> service = [[StrawServiceToast alloc] init];
+
+    XCTAssertFalse([service isBackgroundJob:@"toast"], @"toast method isn't a background job.");
+}
+
+- (void)testToast
+{
+    StrawServiceToast *service = [[StrawServiceToast alloc] init];
+
+    UIViewController *viewController = mock([UIViewController class]);
+
+    [viewController view];
+
+    UIView *view = mock([UIView class]);
+
+    [given([viewController view]) willReturn:view];
+
+    service.viewController = viewController;
+
+    [service toast:@{@"text": @"toast text"} withContext:nil];
+
+    [verifyCount(view, times(1)) makeToast:@"toast text"];
 }
 
 @end
