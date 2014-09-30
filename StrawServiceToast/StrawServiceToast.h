19ab2a3b7df0c02163a88f9d2871c2ff513aab2a
Yoshiya Hinosawa <stibium121@gmail.com>
2014-09-30 23:28:59 +0100

    Add doc

--- StrawServiceToast/StrawServiceToast.h
+++ StrawServiceToast/StrawServiceToast.h
@@ -2,8 +2,17 @@
 #import <Straw/Straw.h>
 #import <UIView+Toast.h>
 
+/**
+ StrawServiceToast is Straw Service class to show Toast UI component on the screen.
+ */
 @interface StrawServiceToast : NSObject <STWService, STWServiceWithViewController>
 
+/**
+ Toast (like Toast in Android) a message.
+
+ @param params Straw Service call params. The value on @"text" key will be toasted on the screen.
+ @param context Straw Service call context of execution. Not used.
+ */
 - (void)toast:(NSDictionary *)params withContext:(id<STWServiceCallContext>)context;
 
 @end
